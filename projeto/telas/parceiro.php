3615d12bf519df6d94a5a5616af13c3611c15761
Fernando Pinage <luizfernandoluck@hotmail.com>
2020-10-04 14:17:43 -0300

    tela cadastro de parceiros

--- projeto/telas/parceiro.php
+++ projeto/telas/parceiro.php
@@ -1,86 +1,335 @@
+<legend class="legend">Cadastro de Parceiros </legend><hr><br><br>
+<nav>
+    <div class="nav nav-tabs" id="nav-tab" role="tablist">
+        <a class="nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">Dados Pessoais</a>
+        <a class="nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false">Endereço</a>
+        <a class="nav-link" id="nav-contact-tab" data-toggle="tab" href="#nav-contact" role="tab" aria-controls="nav-contact" aria-selected="false">Contrato</a>
+        <a class="nav-link" id="nav-contact-tab" data-toggle="tab" href="#nav-dependente" role="tab" aria-controls="nav-dependente" aria-selected="false">Dependentes</a>
+    </div>
+</nav>
 <form>
 
 
-    <div class="form-group">
+<div class="tab-content" id="nav-tabContent">
+    <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
 
-            <div class="custom-control custom-radio">
-                <input type="radio" id="customRadio1" name="customRadio" class="custom-control-input">
-                <label class="custom-control-label" for="customRadio1">Pessoa Fisica</label>
-            </div>
-            <div class="custom-control custom-radio">
-                <input type="radio" id="customRadio2" name="customRadio" class="custom-control-input">
-                <label class="custom-control-label" for="customRadio2">Pessoa Juridica</label>
-            </div>
-        <br>
-        <div class="form-row">
+        <div class="form-group card border" style="padding: 35px 35px 35px 35px; ">
 
-            <div class=" col-md-3">
-                <label for="exampleInputEmail1">Nome:</label>
-                <input type="text" class="form-control" placeholder="Nome Completo">
-            </div>
-            <div class="col-md-3">
-                <label for="exampleInputEmail1">E-mail:</label>
-                <input type="text" class="form-control" placeholder="E-mail">
-            </div>
-            <div class="col-md-2">
-                <label for="exampleInputEmail1">CPF:</label>
-                <input type="text" class="form-control" placeholder="CPF">
-            </div>
-            <div class="col-md-2">
-                <label for="exampleInputEmail1">RG:</label>
-                <input type="text" class="form-control" placeholder="RG">
-            </div>
-            <div class="col-md-2">
-                <label for="exampleInputEmail1">Estado Civil:</label>
-                <input type="text" class="form-control" placeholder="EStado Civil">
-            </div>
+            <div class="form-group row ">
 
-        </div>
-
-        <div class="form-row">
-
-            <div class="col-md-3">
-                <label for="exampleInputEmail1">Data Nascimento:</label>
-                <input type="date" class="form-control" placeholder="Data Nascimento">
-            </div>
-            <div class=" col-md-3">
-                <label for="exampleInputEmail1">Telefone:</label>
-                <input type="text" class="form-control" placeholder="(xx)xxxxx-xxxx">
-            </div>
-
-            <div class=" col-md-3">
-                <label for="exampleInputEmail1">Celular:</label>
-                <input type="text" class="form-control" placeholder="(xx)xxxxx-xxxx">
-            </div>
-
-            <div class=" col-md-3">
-                <label for="exampleInputEmail1">CEP:</label>
-                <input type="text" class="form-control" placeholder="CEP">
-            </div>
+                <div class=" col-md-6">
+                    <label for="inputEmail4">Nome Completo:</label>
+                    <input type="text" name='nome' class="form-control" required="true">
+                </div>
+                <div class="col-md-6">
+                    <label for="inputEmail4">Email:</label>
+                    <input type="text" name='email' class="form-control" required="true">
+                </div>
 
 
+            </div>
 
-        </div>
+            <div class="form-group row">
 
-        <div class="form-row">
-            <div class="col-md-2">
-                <label for="exampleInputEmail1">Cidade:</label>
-                <input type="text" class="form-control" placeholder="Cidade">
+
+                <div class=" col-md-3">
+                    <label for="inputEmail4">CPF:</label>
+                    <input type="text" name='cpf' class="form-control" oninput="mascara(this, 'cpf')" maxlength='14' required="true">
+                </div>
+                <div class="col-md-3">
+                    <label for="inputEmail4">RG:</label>
+                    <input type="text" name='rg' id='rg' class="form-control" maxlength='10' required="true">
+                </div>
+                <div class=" col-md-3">
+                    <label for="inputEmail4">Celular:</label>
+                    <input type="text" name='celular' class="form-control" oninput="mascara(this, 'tel')">
+                </div>
+                <div class="col-md-3">
+                    <label for="inputEmail4">Telefone:</label>
+                    <input type="text" name='telefone' class="form-control" oninput="mascara(this, 'tel')">
+                </div>
+
             </div>
-            <div class="col-md-1">
-                <label for="exampleInputEmail1">UF:</label>
-                <input type="text" class="form-control" placeholder="UF">
-            </div>
-            <div class="col-md-4">
-                <label for="exampleInputEmail1">Bairro:</label>
-                <input type="text" class="form-control" placeholder="Bairro">
-            </div>
-            <div class="col-md-4">
-                <label for="exampleInputEmail1">Complemento:</label>
-                <input type="text" class="form-control" placeholder="Complemento">
-            </div>
+
         </div>
 
     </div>
+        <!-- ENDEREÇO -->
+    <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
 
+        <div class="form-group card border" style="padding: 35px 35px 35px 35px; ">
+
+            <div class="form-group row">
+
+                <div class=" col-md-3">
+                    <label for="inputEmail4">CEP:</label>
+                    <input type="text" name="cep" id="cep" class="form-control" oninput="mascara(this, 'cep')">
+                </div>
+                <div class="col-md-1">
+                    <label for="inputEmail4">Nº:</label>
+                    <input type="text" name='numero' class="form-control">
+                </div>
+                <div class="col-md-1">
+                    <label for="inputEmail4">UF:</label>
+                    <input type="text" name='uf' id='uf' class="form-control">
+                </div>
+                <div class="col-md-3">
+                    <label for="inputEmail4">Cidade:</label>
+                    <input type="text" name="cidade" id='cidade' class="form-control">
+                </div>
+                <div class="col-md-4">
+                    <label for="inputEmail4">Logradouro:</label>
+                    <input type="text" name="logradouro" id='rua' class="form-control">
+                </div>
+
+
+            </div>
+
+            <div class="form-group row">
+
+
+                <div class=" col-md-4">
+                    <label for="inputEmail4">Bairro:</label>
+                    <input type="text" name="bairro" id='bairro' class="form-control">
+                </div>
+                <div class="col-md-8">
+                    <label for="inputEmail4">Complemento:</label>
+                    <input type="text" name="complemento" class="form-control">
+                </div>
+
+            </div>
+
+
+        </div>
+    </div>
+        <!-- CONTRATO -->
+    <div class="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">
+
+        <div class="form-group card border" style="padding: 35px 35px 35px 35px; ">
+
+            <div class="form-group row">
+
+                <div class=" col-md-3">
+                    <label for="inputEmail4">Tipo:</label>
+                    <select class="custom-select" name="tipo" id="inputGroupSelect01">
+                        <option value="0"></option>
+                        <option value="1">ANUAL</option>
+                        <option value="2">MENSAL</option>
+
+                    </select>
+                </div>
+                <div class="col-md-3">
+                    <label for="inputEmail4">Fim de Contrato:</label>
+                    <input type="date" name="fimcont" class="form-control">
+                </div>
+                <div class="col-md-3">
+                    <label for="inputEmail4">Pagamento:</label>
+                    <select class="custom-select" name="pagamento" id="inputGroupSelect01">
+                        <option value="0"></option>
+                        <option value="1">DIA 01</option>
+                        <option value="2">DIA 05</option>
+                        <option value="2">DIA 10</option>
+                        <option value="2">DIA 15</option>
+                        <option value="2">DIA 20</option>
+                        <option value="2">DIA 25</option>
+                    </select>
+                </div>
+                <div class="col-md-3">
+                    <label for="inputEmail4">Forma de Pagamento:</label>
+                    <select class="custom-select" name="forpag" id="inputGroupSelect01">
+                        <option value="0"></option>
+                        <option value="1">AVISTA</option>
+                        <option value="2">PARCELADO</option>
+
+                    </select>
+                </div>
+
+            </div>
+
+        </div>
+    </div>
+    <!-- DEPENDENTE -->
+    <div class="tab-pane fade" id="nav-dependente" role="tabpanel" aria-labelledby="nav-dependente-tab">
+        <div class="form-group card border" style="padding: 35px 35px 35px 35px; ">
+
+            <div class="form-group row">
+
+                <div class=" col-md-4">
+                    <label for="inputEmail4">Dependente:</label>
+                    <input type="text" name="dep1" class="form-control">
+                </div>
+                <div class=" col-md-4">
+                    <label for="inputEmail4">Dependente:</label>
+                    <input type="text" name="dep2" class="form-control">
+                </div>
+                <div class=" col-md-4">
+                    <label for="inputEmail4">Dependente:</label>
+                    <input type="text" name="dep3" class="form-control">
+                </div>
+
+
+
+            </div>
+            <div class="form-group row">
+
+                <div class=" col-md-4">
+                    <label for="inputEmail4">Dependente:</label>
+                    <input type="text" name="dep4" class="form-control">
+                </div>
+                <div class=" col-md-4">
+                    <label for="inputEmail4">Dependente:</label>
+                    <input type="text" name="dep5" class="form-control">
+                </div>
+                <div class=" col-md-4">
+                    <label for="inputEmail4">Dependente:</label>
+                    <input type="text" name="dep6" class="form-control">
+                </div>
+
+            </div>
+            <div class="form-group row">
+
+                <div class=" col-md-4">
+                    <label for="inputEmail4">Dependente:</label>
+                    <input type="text" name="dep7" class="form-control">
+                </div>
+                <div class=" col-md-4">
+                    <label for="inputEmail4">Dependente:</label>
+                    <input type="text" name="dep8" class="form-control">
+                </div>
+                <div class=" col-md-4">
+                    <label for="inputEmail4">Dependente:</label>
+                    <input type="text" name="dep9" class="form-control">
+                </div>
+
+            </div>
+
+        </div>
+    </div>
+</div>
+<div class="form-group row">
+    <input class="btn btn-primary" type="submit" value='SALVAR'>
+
+</div>
 </form>
+<!-- VIA CEP -->
+
+
+<script>
+
+$(document).ready(function() {
+
+    function limpa_formulário_cep() {
+        // Limpa valores do formulário de cep.
+        $("#rua").val("");
+        $("#bairro").val("");
+        $("#cidade").val("");
+        $("#uf").val("");
+        $("#ibge").val("");
+    }
+    
+    //Quando o campo cep perde o foco.
+    $("#cep").blur(function() {
+
+        //Nova variável "cep" somente com dígitos.
+        var cep = $(this).val().replace(/\D/g, '');
+
+        //Verifica se campo cep possui valor informado.
+        if (cep != "") {
+
+            //Expressão regular para validar o CEP.
+            var validacep = /^[0-9]{8}$/;
+
+            //Valida o formato do CEP.
+            if(validacep.test(cep)) {
+
+                //Preenche os campos com "..." enquanto consulta webservice.
+                $("#rua").val("...");
+                $("#bairro").val("...");
+                $("#cidade").val("...");
+                $("#uf").val("...");
+                $("#ibge").val("...");
+
+                //Consulta o webservice viacep.com.br/
+                $.getJSON("https://viacep.com.br/ws/"+ cep +"/json/?callback=?", function(dados) {
+
+                    if (!("erro" in dados)) {
+                        //Atualiza os campos com os valores da consulta.
+                        $("#rua").val(dados.logradouro);
+                        $("#bairro").val(dados.bairro);
+                        $("#cidade").val(dados.localidade);
+                        $("#uf").val(dados.uf);
+                        $("#ibge").val(dados.ibge);
+                    } //end if.
+                    else {
+                        //CEP pesquisado não foi encontrado.
+                        limpa_formulário_cep();
+                        alert("CEP não encontrado.");
+                    }
+                });
+            } //end if.
+            else {
+                //cep é inválido.
+                limpa_formulário_cep();
+                alert("Formato de CEP inválido.");
+            }
+        } //end if.
+        else {
+            //cep sem valor, limpa formulário.
+            limpa_formulário_cep();
+        }
+    });
+});
+
+</script>
+
+
+<!-- mascara de campos -->
+
+    <script type="text/javascript">
+			function mascara(i,t){
+   
+        var v = i.value;
+        
+        if(isNaN(v[v.length-1])){
+            i.value = v.substring(0, v.length-1);
+            return;
+        }
+        
+        if(t == "data"){
+            i.setAttribute("maxlength", "10");
+            if (v.length == 2 || v.length == 5) i.value += "/";
+        }
+
+        if(t == "cpf"){
+            i.setAttribute("maxlength", "14");
+            if (v.length == 3 || v.length == 7) i.value += ".";
+            if (v.length == 11) i.value += "-";
+        }
+
+        if(t == "cnpj"){
+            i.setAttribute("maxlength", "18");
+            if (v.length == 2 || v.length == 6) i.value += ".";
+            if (v.length == 10) i.value += "/";
+            if (v.length == 15) i.value += "-";
+        }
+
+        if(t == "cep"){
+            i.setAttribute("maxlength", "9");
+            if (v.length == 5) i.value += "-";
+        }
+
+        if(t == "tel"){
+            if(v[0] == 9){
+                i.setAttribute("maxlength", "10");
+                
+                if (v.length == 5) i.value += "-";
+            }else{
+                i.setAttribute("maxlength", "9");
+                if (v.length == 4) i.value += "-";
+                }
+            }
+   
+    }
+
+
+		</script>
