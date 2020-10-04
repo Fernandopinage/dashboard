9f116ad4387acec38588df4fe33fa154c73c6d65
Fernando Pinage <luizfernandoluck@hotmail.com>
2020-10-04 14:24:40 -0300

    valindando cep

--- projeto/telas/home.php
+++ projeto/telas/home.php
@@ -3,6 +3,11 @@
 <head>
     <link rel="stylesheet" href="../css/home.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+     <!-- Adicionando JQuery -->
+     <script src="https://code.jquery.com/jquery-3.4.1.min.js"  integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="  crossorigin="anonymous"></script>
+
+<!-- Adicionando Javascript -->
+
 </head>
 
 <body>
